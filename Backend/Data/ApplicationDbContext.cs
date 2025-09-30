17c5247e70920187ec99a034478ff9898dd5f6d9
Thiahho <thiagobrizuelav@gmail.com>
2025-09-29 21:53:14 -0300

    29092025 Agregado el Backend, falta la sincronizacion

---
+++ Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,54 @@
+﻿using OrigamiBack.Data.Modelos;
+using OrigamiBack.Data.Vistas;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrigamiBack.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+             : base(options) { }
+
+        public DbSet<Celular> Celulares { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+        public DbSet<ProductosVariantes> ProductosVariantes { get; set; }
+
+        /// VISTAS
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<Usuario>()
+                .ToTable("usuarios")
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Celular>()
+                .ToTable("celulares")
+                .HasKey(e => e.id);
+
+          modelBuilder.Entity<Categorias>()
+                .ToTable("categorias")
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Productos>()
+                .ToTable("productos")
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Productos>()
+                .HasMany(p => p.Variantes)
+                .WithOne(v => v.Producto)
+                .HasForeignKey(v => v.ProductoId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            modelBuilder.Entity<ProductosVariantes>()
+                .ToTable("productos_variantes")
+                .HasKey(v => v.Id);
+        
+          
+        }
+    }
+}
---
+++ Backend/Data/MappingProfile.cs
@@ -0,0 +1,22 @@
+using AutoMapper;
+using OrigamiBack.Data.Modelos;
+using OrigamiBack.Data.Dtos;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // Entidad -> DTO
+        CreateMap<Productos, ProductoDto>().ReverseMap();
+        CreateMap<ProductosVariantes, ProductosVariantesDto>().ReverseMap();
+        // Agrega aquí todos los mapeos que necesites
+        CreateMap<ProductoDto, Productos>()
+            .ForMember(dest => dest.Img, opt => opt.MapFrom(src =>
+                string.IsNullOrEmpty(src.Img) ? null : Convert.FromBase64String(src.Img)));
+
+        CreateMap<Productos, ProductoDto>()
+            .ForMember(dest => dest.Img, opt => opt.MapFrom(src =>
+                src.Img != null ? Convert.ToBase64String(src.Img) : null));
+
+    }
+}
